54f35fb54981b7b61b71f61cf056311b712b427f
Locke-G <gabriellocke@outlook.com>
2022-12-29 18:03:28 +0100

    fixed bug where Database would not check for Username

--- ASR - Airline Reservation WebApp/Project/app/auth.py
+++ ASR - Airline Reservation WebApp/Project/app/auth.py
@@ -22,7 +22,8 @@ def login_post():
     # take the user supplied password, hash it, and compare it to the hashed password in database
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
+        # if user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login'))
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
@@ -36,17 +37,27 @@ def signup():
 def signup_post():
 
     email = request.form.get('email')
+    username = request.form.get('username')
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    # if this returns a userERx, then the email
+    # or username already exists in database
+    userER1 = User.query.filter_by(email=email).first()
+    userER2 = User.query.filter_by(username=username).first()
 
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists')
+    if userER1: # if an email is found, we want to redirect back to signup
+        flash('Email address already exists. Try again.')
         return redirect(url_for('auth.signup'))
 
-    # create new user with the form data. Hash the password so plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    if userER2: # if a user is found, we want to redirect back to signup
+        flash('Username already exists. Try again.')
+        return redirect(url_for('auth.signup'))
+
+    # create new user with the form data.
+    # Hash the password so plaintext version isn't saved.
+    new_user = User(email=email, username=username, name=name,
+                    password=generate_password_hash(password, method='sha256'))
 
     # add the new user to the database
     db.session.add(new_user)
--- ASR - Airline Reservation WebApp/Project/app/models.py
+++ ASR - Airline Reservation WebApp/Project/app/models.py
@@ -3,6 +3,7 @@ from . import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
