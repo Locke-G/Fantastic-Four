87034bc4108feb9cd9e28193edecc0f6b2063316
Locke-G <gabriellocke@outlook.com>
2023-01-24 18:50:26 +0100

    IT WORKS ... Seat display system logic is working. Getting and displaying data from the db

--- ASR - Airline Reservation WebApp/Project/app/main.py
+++ ASR - Airline Reservation WebApp/Project/app/main.py
@@ -46,7 +46,13 @@ def upload():
                     seat_id = str(i + 1) + chr(ord('A') + j)
                     row = int(seat_id[:-1])
                     column = seat_id[-1]
-                    seat = Seat(seat_id=seat_id, status='available', airline=root, row=row, column=column)
+                    seat = Seat(
+                        seat_id=seat_id,
+                        status='available',
+                        airline=root,
+                        row=row,
+                        column=column
+                    )
                     db.session.add(seat)
             db.session.commit()
         flash('File uploaded and processed')
@@ -54,32 +60,35 @@ def upload():
     return render_template('booking/upload.html')
 
 
-@main.route('/select_airline', methods=['GET', 'POST'])
-def select_airline():
+@main.route('/seats', methods=['GET', 'POST'])
+@login_required
+def seats():
+    airlines = db.session.query(Seat.airline).distinct().all()
     if request.method == 'POST':
-        selected_airline = request.form.get('airline_select')
-        seats = Seat.query.filter_by(airline=selected_airline).all()
-        max_row = max(seat.row for seat in seats)
-        max_col = max(ord(seat.column) - ord('A') for seat in seats)
-        seat_chart = [['' for _ in range(max_col+1)] for _ in range(max_row+1)]
-        for seat in seats:
-            seat_chart[seat.row-1][ord(seat.column) - ord('A')] = seat.seat_id
+        airline = request.form.get('airline')
+        seats = Seat.query.filter_by(airline=airline).all()
+        rows = db.session.query(Seat.row).distinct().filter_by(airline=airline).all()
+        columns = db.session.query(Seat.column).distinct().filter_by(airline=airline).all()
     else:
-        selected_airline = db.session.query(Seat.airline).distinct().first()[0]
-        seats = Seat.query.filter(Seat.airline == selected_airline).all()
-        max_row = max(seat.row for seat in seats)
-        max_col = max(ord(seat.column) - ord('A') for seat in seats)
-        #TypeError: can only concatenate str (not "int") to str
-        seat_chart = [['' for _ in range(max_col+1)] for _ in range(max_row+1)]
-        for seat in seats:
-            seat_chart[seat.row-1][ord(seat.column) - ord('A')] = seat.seat_id
+        airlines = db.session.query(Seat.airline).distinct().all()
+        seats = []
+        rows = []
+        columns = []
+    return render_template('booking/seats.html', airlines=airlines, seats=seats, rows=rows, columns=columns)
 
-    airlines = db.session.query(Seat.airline).distinct()
-    return render_template(
-        'booking/select_airline.html',
-        airlines=airlines,
-        seat_chart=seat_chart,
-        selected_airline=selected_airline
-    )
+
+
+import logging
+
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
+# Use the logging module to log a message
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+
+
+
 
 
