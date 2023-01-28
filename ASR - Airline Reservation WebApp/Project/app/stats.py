16240196085a12b64697b1e970072d3dc790c458
Locke-G <gabriellocke@outlook.com>
2023-01-28 09:41:14 +0100

    -

--- ASR - Airline Reservation WebApp/Project/app/stats.py
+++ ASR - Airline Reservation WebApp/Project/app/stats.py
@@ -6,7 +6,7 @@ from . import db
 from matplotlib import pyplot as plt
 import io
 import base64
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import pandas as pd
 
 stats = Blueprint('stats', __name__)
 def role_required(role):
@@ -18,8 +18,6 @@ def role_required(role):
             return f(*args, **kwargs)
         return decorated_function
     return decorator
-
-
 
 @stats.route('/graph1', methods=['GET', 'POST'])
 @login_required
@@ -36,12 +34,45 @@ def graph_seats():
         reserved_seats[airline] = reserved
         available_seats[airline] = available
     fig, ax = plt.subplots()
+    plt.subplots_adjust(left=0.2, bottom=0.3)
     ax.bar(reserved_seats.keys(), reserved_seats.values(), label="Reserved")
     ax.bar(available_seats.keys(), available_seats.values(), label="Available")
     ax.legend()
+
+    # Create table
+    table_data = [['Airline', 'Reserved Seats', 'Available Seats']]
+    cellText = []
+    for airline, reserved in reserved_seats.items():
+        available = available_seats[airline]
+        cellText.append([airline, reserved, available])
+    table = ax.table(cellText=cellText, colLabels=["Airline", "Reserved", "Available"], loc='bottom')
+    table.auto_set_font_size(False)
+    table.set_fontsize(8)
+    table.scale(1, 1.5)
+
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
     buf.seek(0)
     imdata = base64.b64encode(buf.read()).decode("ascii")
-    return render_template("graph.html", mimetype="buf/png", cache_timeout=0, imdata=imdata)
+
+    return render_template("stats/graph.html", mimetype="buf/png", cache_timeout=0, imdata=imdata)
+
+@stats.route('/tables', methods=['GET', 'POST'])
+@login_required
+@role_required('admin')
+def tables():
+    airlines = db.session.query(Seat.airline).distinct().all()
+    airlines = [airline[0] for airline in airlines]
+    reserved_seats = {}
+    available_seats = {}
+    for airline in airlines:
+        reserved = Seat.query.filter(Seat.status == 'reserved', Seat.airline == airline).all()
+        available = Seat.query.filter(Seat.status == 'available', Seat.airline == airline).all()
+        reserved_seats[airline] = reserved
+        available_seats[airline] = available
+    return render_template("stats/table_seats.html", reserved_seats=reserved_seats, available_seats=available_seats)
+
+
+
+
 
